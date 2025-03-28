0255b7d20861d638c6a41123830352c60fa04157
Aayush Gauba <55630226+aayushgauba@users.noreply.github.com>
2025-03-27 20:36:13 -0500

    Bug Fixes

--- se3mar8.py
+++ se3mar8.py
@@ -293,7 +293,7 @@ cfg_seg = all_cfg.seg
 # %%
 wd = os.path.join("experiments", "mug", "pick")
 os.makedirs(wd, exist_ok=True)
-demo_path = os.path.join("data", "mug", "pick", "demos.npz")
+demo_path = os.path.join("data", "mug", "pick", "demo.npz")
 
 # %%
 
